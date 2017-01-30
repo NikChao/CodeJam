cedd829a288308ab43b508c9bcbef08605058939
NikChao <nik.chaourov@gmail.com>
2017-01-30 18:24:08 +1000

    added example field to problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129041927) do
+ActiveRecord::Schema.define(version: 20170130063951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170129041927) do
     t.datetime "updated_at",     null: false
     t.integer  "points"
     t.string   "difficulty"
+    t.string   "example"
     t.index ["competition_id"], name: "index_problems_on_competition_id", using: :btree
   end
 
